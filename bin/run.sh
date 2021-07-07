3833eb11c1031edd08baf178a4e3eb0bec4134a0
Maisam Somji <maisam.somji@gmail.com>
2021-07-07 05:15:55 -0400

    update sed regex to delete Ignore annotation with meessage or without (#20)

--- bin/run.sh
+++ bin/run.sh
@@ -15,7 +15,7 @@ compiler_error_log="compiler.log"
 cp -r $input_folder /opt/test-runner/lib/$problem_slug
 cd /opt/test-runner/lib
 
-find . -mindepth 1 -type f | grep 'Test.java' | xargs -I file sed -i "s/@Ignore/\n/g" file
+find . -mindepth 1 -type f | grep 'Test.java' | xargs -I file sed -i "s/@Ignore(.*)//g;s/@Ignore//g;" file
 cat <<EOF >settings.gradle
 include '${problem_slug}'
 EOF
