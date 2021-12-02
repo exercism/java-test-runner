ff721b38e645c5614b4ee67b0bacc7362439d37b
andreaTP <andrea.peruffo1982@gmail.com>
2021-12-02 17:47:52 +0000

    Run tests on Java 17

---
+++ tests/example-success-java17/src/main/java/Leap.java
@@ -0,0 +1,13 @@
+record Leap() {
+
+    boolean isLeapYear(int year) {
+        return (
+            (
+                (year % 4 == 0) && 
+                (year % 100 != 0)
+            ) || 
+            (year % 400 == 0)
+            ); 
+    }
+
+}
