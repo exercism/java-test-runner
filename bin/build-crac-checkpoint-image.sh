37de8233d2b4b7ae8c78200f67e54c0aaf874fac
fap <459631+fapdash@users.noreply.github.com>
2025-03-11 13:19:22 +0100

    Improve test runner performance by running from CRaC checkpoint
    The test runner suffers from the slow startup time of the JVM.
    My goal with this PR was to significantly improve the performance
    of the Java test runner.
    
    There are at the time of writing several options to improve JVM
    startup time:
    1. Graal native image (AOT)
    2. Shared AppCDS
    3. Project Leyden
    4. CRaC
    
    Native image can't be used for the test runner as it has to
    dynamically load classes at runtime and Graal AOT depends on a closed
    world assumption.
    
    Shared AppCDS improves performance. In my testing a test run did go
    down from ~4s to ~3s.
    
    Project Leyden is very promising, as it tries to do as much work
    as possible ahead of time without making a closed world assumption.
    At the time of this writing the project is only in early access,
    so it's probably going to take a while before it lands in a LTS
    release.
    
    CRaC brings the best performance improvements, but with some caveats:
    - build gets more complicated
    - the CRIU based engine needs to run with two additional capabilities:
        - CHECKPOINT_RESTORE
        - SYS_PTRACE
    - performance speedup depends on how well the JVM gets warmed up
      before the checkpoint is taken
    
    bin/run-tests-in-docker.sh had to be adjusted to start a new
    container for each test. The restored JVM needs to be run as a
    specific PID, so it can only be restored once per container life
    cycle.
    The test run still finishes faster than before.
    
    This commit uses the CRIU engine as I had some issues getting the warp
    engine to work properly. The warp engine is also only supported by
    Azul right now and isn't compatible with musl / Alpine yet.
    In my tests the runtime of my example test did go down from ~4s to
    >1s.
    By switching to an Alpine based image this change also reduces to size
    of the container (exercism/java-test-runner-crac-checkpoint) to 271MB,
    down from previously 464MB.
    
    CRaC documentation:
    - https://crac.org/
    - https://docs.azul.com/core/crac/crac-introduction
    - https://openjdk.org/projects/crac/

---
+++ bin/build-crac-checkpoint-image.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+# Synopsis:
+# Build a Docker image containing a CraC checkpoint to restart from.
+# An initial image is built. A container is created from that image
+# and tests are run to warm up the JVM. Then a checkpoint is created.
+# The final image is created by committing the containiner
+# containing the checkpoint.
+
+docker build -t exercism/java-test-runner-crac-checkpoint .
+
+# Copy all tests into one merged project, so we can warm up the JVM
+# TODO(FAP): this is missing some tests as most tests use the same filenames
+mkdir -p tests/merged
+for dir in tests/*; do
+    if [ -d "$dir" ] && [ "$dir" != "tests/merged/" ]; then
+        rsync -a "$dir"/ tests/merged/
+    fi
+done
+
+slug="merged"
+solution_dir=$(realpath "tests/merged/")
+output_dir=$(realpath "tests/merged/")
+
+docker run --cap-add CHECKPOINT_RESTORE \
+           --cap-add SYS_PTRACE \
+           --name java-test-runner-crac \
+           --network none \
+           --mount type=bind,src="${solution_dir}",dst=/solution \
+           --mount type=bind,src="${output_dir}",dst=/output \
+           --mount type=tmpfs,dst=/tmp \
+           --tmpfs /openjfx:exec,rw \
+           exercism/java-test-runner-crac-checkpoint "${slug}" /solution /output
+
+docker commit --change='ENTRYPOINT ["sh", "/opt/test-runner/bin/run-restore-from-checkpoint.sh"]' java-test-runner-crac exercism/java-test-runner-crac-restore
+
+docker rm -f java-test-runner-crac
+rm -rf tests/merged/
